fd5d0df85d8ccb7954e73143649340697890e6c7
souparno-nag <souparnonag2004@gmail.com>
2026-01-30 09:47:22 +0530

    pre exam practice

---
+++ system-calls/if_else_fork_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    if (fork()) {
+        if (!fork()) {
+            fork();
+            printf("1");
+        } else {
+            printf("2");
+        }
+    } else {
+        printf("3");
+    }
+    printf("\n");
+    return 0;
+}
